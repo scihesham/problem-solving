0b44939e768371337ac0cde66a458dfffbf38401
local-dev <scihesham12@gmail.com>
2023-07-05 05:28:53 +0300

    queue-linked-list

---
+++ queue/queue-linked.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+require_once '../pretty.php';
+require_once '../single-linked-list/single-linked-list.php';
+
+class Queue{
+    private LinkedList $list;
+
+    function __construct() {
+        $this->list = new LinkedList();
+    }
+
+    public function enqueue(mixed $value): void{
+        $this->list->insertEnd($value);
+    }
+
+    public function dequeue(): void{
+        $this->list->deleteNthNode(1);
+    }
+
+    public function display(): void{
+        $this->list->print();
+    }
+
+}
+
+
+$queue = new Queue();
+$queue->enqueue(10);
+$queue->enqueue(20);
+$queue->dequeue();
+$queue->enqueue(200);
+$queue->enqueue(300);
+$queue->enqueue(700);
+$queue->enqueue(800);
+$queue->display();
